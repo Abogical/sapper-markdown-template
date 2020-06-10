4b5d001382f8edbef5240e44c8e2bac1721a81d3
Abdel-Rahman Abdel-Rahman <abogical@gmail.com>
2020-06-10 04:58:53 +0200

    Setup build github action for Abogical/sapper-markdown-template
    Removes email and name association with svelte on the bot and build
    on this template's repositories.

--- _template/build.sh
+++ _template/build.sh
@@ -3,8 +3,8 @@ cd "$(dirname "$0")"
 
 if [ "$CI" ]; then
 	(umask 0077; echo "$SSH_KEY" > ~/ssh_key; echo "$SSH_KEY_ROLLUP" > ~/ssh_key_rollup; echo "$SSH_KEY_WEBPACK" > ~/ssh_key_webpack)
-	git config user.email 'noreply@svelte.dev'
-	git config user.name '[bot]'
+	git config user.email 'abogical@gmail.com'
+	git config user.name 'Abogical Bot'
 fi
 
 # branch names
@@ -14,6 +14,6 @@ WEBPACK=webpack
 ./create-branches.sh $ROLLUP $WEBPACK
 
 # force push rollup and webpack branches and repos
-GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key' git push git@github.com:sveltejs/sapper-template.git $ROLLUP $WEBPACK -f
-GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_rollup' git push git@github.com:sveltejs/sapper-template-rollup.git $ROLLUP:master -f
-GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_webpack' git push git@github.com:sveltejs/sapper-template-webpack.git $WEBPACK:master -f
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key' git push git@github.com:${GITHUB_REPOSITORY}.git $ROLLUP $WEBPACK -f
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_rollup' git push git@github.com:${GITHUB_REPOSITORY}-rollup.git $ROLLUP:master -f
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_webpack' git push git@github.com:${GITHUB_REPOSITORY}-webpack.git $WEBPACK:master -f
